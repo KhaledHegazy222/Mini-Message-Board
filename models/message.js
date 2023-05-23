bb7253e67fa0d9db0a7a3179e71fa2b85218d043
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-05-23 21:58:05 +0300

    Fix Time in Messages

--- models/message.js
+++ models/message.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 messageSchema = new mongoose.Schema({
   name: { type: String, required: true },
   text: { type: String, require: true },
-  sentAt: { type: Date, default: Date.now() },
+  sentAt: { type: Date, default: () => Date.now() },
 });
 
 module.exports = mongoose.model("Message", messageSchema);
